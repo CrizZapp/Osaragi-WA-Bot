e149fc8886de96afd3499f5e7d5fea8ebff2de75
CrizZapp <juegoscris564@gmail.com>
2026-06-18 14:39:20 -0300

    Actualización pa

--- plugins/menu.js
+++ plugins/menu.js
@@ -7,31 +7,73 @@ const miidPlugin = async (m, { conn, from, sender }) => {
   const cards = [
     {
       title: "*〇ׁ ˳ּ𑁍 ׁ✹ ⋆☤ ᗰᗴᑎᑌ ♕ OՏᗩᘜᗩᖇI ☤⋆ ✺˳ּ ׁ〇*",
-      text: `👤 Usuario: ${userName}\n\n🧸 Grupo: ${groupName}`,
+      text: `👤 Usuario: ${userName}\n\n🆔 Identificador:\n${sender}\n\n🧸Grupo: ${groupName}`,
       footer: "𝓞𝓈𝓪𝓰𝖆𝖗𝖎 • 2026",
       image: './banner.jpg', 
-     
+   
       buttons: [
         {
-          type: "quick_reply",
-          params: {
-            display_text: "🧨 𝐋𝐚𝐭𝐞𝐧𝐜𝐢𝐚 ✨️",
-            id: "#ping"
+          type: "cta_copy",
+          params: { 
+            display_text: "📋 𝐓𝐨𝐜𝐚 𝐩𝐚𝐫𝐚 𝐜𝐨𝐩𝐢𝐚𝐫", 
+            copy_code: "Allen es God :D"
+          }
+        },
+       {
+          type: "cta_copy",
+          params: { 
+            display_text: "🧸 𝐋𝐚𝐭𝐞𝐧𝐜𝐢𝐚 🧸", 
+            copy_code: "#ping"
           }
         },
         {
           type: "cta_copy",
-          params: {
-            display_text: "📋 𝐂𝐨𝐩𝐢𝐚𝐫",
-            copy_code: "Allen es God :D"
+          params: { 
+            display_text: "🕹 𝐄𝐬𝐭𝐞 𝐦𝐞𝐧𝐮 🕹", 
+            copy_code: "#menu"
+          }
+        },
+        {
+          type: "cta_copy",
+          params: { 
+            display_text: "🕹 𝐏𝐢𝐧𝐞𝐫𝐞𝐬𝐭 𝐬𝐞𝐚𝐫𝐜𝐡 🕹", 
+            copy_code: "#pin"
+          }
+        },
+         {
+          type: "cta_copy",
+          params: { 
+            display_text: "🕹 𝐌𝐮𝐬𝐢𝐜𝐚 (𝐀𝐮𝐝𝐢𝐨)🕹", 
+            copy_code: "#playaudio"
+          }
+        },
+        {
+          type: "cta_copy",
+          params: { 
+            display_text: "🕹 𝐌𝐮𝐬𝐢𝐜𝐚 (𝐕𝐢𝐝𝐞𝐨) 🕹", 
+            copy_code: "#play"
+          }
+        },
+        {
+          type: "cta_copy",
+          params: { 
+            display_text: "🕹 𝐒𝐞𝐫 𝐛𝐨𝐭 🕹", 
+            copy_code: "desactivado por el creador"
+          }
+        },
+        {
+          type: "cta_copy",
+          params: { 
+            display_text: "🧸✨️ 𝐈𝐀✨️", 
+            copy_code: "#ia"
           }
         },
         {
           type: "cta_url",
-          params: {
-            display_text: "🕹 𝐂𝐚𝐧𝐚𝐥 🕹",
-            url: "https://whatsapp.com/channel/0029VbDdtHHBPzjeRStWI41P",
-            merchant_url: "https://whatsapp.com/channel/0029VbDdtHHBPzjeRStWI41P"
+          params: { 
+            display_text: "🕹 𝐂𝐚𝐧𝐚𝐥 🕹", 
+            url: "https://whatsapp.com/channel/0029VbDdtHHBPzjeRStWI41P", 
+            merchant_url: "https://whatsapp.com/channel/0029VbDdtHHBPzjeRStWI41P" 
           }
         }
       ]
@@ -47,6 +89,7 @@ const miidPlugin = async (m, { conn, from, sender }) => {
   );
 };
 
-miidPlugin.command = ['menu', 'help'];
+
+miidPlugin.command = ['help', 'menu'];
 
 export default miidPlugin;
